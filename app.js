e07ca6914db914aa3eb782c8dec69e71951e7c20
saldivar <evelinesaldivar@gmail.com>
2024-09-27 13:38:02 +0800

    added Conditioal statements

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ let boolean = true;
 // Arithmetic and Logical Operators
 let result = number * 5 + 2;
 let isTrue = number > 5 && boolean;
+
+// Conditional Statements
+// if else
+if (number % 2 === 0) {
+    console.log("Number is even");
+} else {
+    console.log("Number is odd");
+}
+
+// switch
+let day = "Monday";
+switch (day) {
+    case "Monday":
+        console.log("It's Monday");
+        break;
+    case "Tuesday":
+        console.log("It's Tuesday");
+        break;
+    default:
+        console.log("It's another day");
+}
