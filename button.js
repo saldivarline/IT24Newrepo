9eb721bc5e686fb33e547f5d53d868f8c48ff5f5
KennethLicuanan <licuanankennethlloyd05@gmail.com>
2024-10-07 16:24:13 +0800

    added card container method

--- button.js
+++ button.js
@@ -19,4 +19,22 @@ class DataLogger{
         this.loggedData = []; 
         this.updateCardContainer();
     }
+
+    updateCardContainer(){
+        this.idContainer.innerHTML = '';
+    
+        this.loggedData.forEach(data => {       
+            const card = document.createElement('div');
+            card.className = 'card mb-2';
+            card.innerHTML = `
+                <div class="card-body">
+                    <h5 class="card-title">Logged Data</h5>
+                    <p class="card-text">${data}</p>
+                </div>
+            `;
+            this.idContainer.appendChild(card);
+        });
+        
+        this.displayLogCount();
+    }
 }
