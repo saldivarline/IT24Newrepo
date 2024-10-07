d7e70bf73c78e8f248f7f299f559b29c933fb8c9
KennethLicuanan <licuanankennethlloyd05@gmail.com>
2024-10-07 13:38:34 +0800

    added InitTileLayer

--- applet-2.js
+++ applet-2.js
@@ -5,4 +5,11 @@ class LeafletMap {
         this.initTileLayer();
     }
 
-  
+    initTileLayer() {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+    }
+
+}
