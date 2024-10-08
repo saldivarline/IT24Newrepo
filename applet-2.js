b33a79e50b0628f9d3170f830e920e4efbd3420b
saldivarline <evelinesaldivar@gmail.com>
2024-10-08 18:51:15 +0800

    added btn

--- applet-2.js
+++ applet-2.js
@@ -4,15 +4,29 @@ class LeafletMap {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
 
-        this.attendanceCountSC = 0;
-        this.attendanceCountBA = 0;
-        this.attendanceCountLab = 0;
+        this.attendanceCountBNL = 0;
+        this.attendanceCountAS = 0;
+        this.attendanceCountDS = 0;
 
         this.markerCounts = {};
         this.markers = [];
 
         this.loggedData = []; 
+
+        
+        this.btn = document.getElementById('btn');
+        this.btn1 = document.getElementById('btn1');
+        this.btn2 = document.getElementById('btn2');
+        this.btnclear = document.getElementById('btnclear');
+        this.logCountElement = document.getElementById('logCountBNL');
+        this.logCount1Element = document.getElementById('logCountAS');
+        this.logCount2Element = document.getElementById('logCountDS');
+        this.idContainer = document.getElementById('logContainer');
+
+      
+
     }
+
 
     initTileLayer() {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
