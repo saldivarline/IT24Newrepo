1d392b8a52e2c10cc4070f9aef353d00a6296c00
saldiii <evelinesaldivar@gmail.com>
2024-10-11 19:00:41 +0800

    added filterStudents

--- elements.js
+++ elements.js
@@ -41,5 +41,20 @@ class StudentList {
 
         this.renderStudentList(this.students, studentSearchListContainer);
     }
+
+
+    filterStudents(query, searchListContainer) {
+        const filteredStudents = this.students.filter(student => {
+            const fullName = `${student.student_name} ${student.student_program}`;
+            return fullName.toLowerCase().includes(query.toLowerCase());
+        });
+
+        searchListContainer.innerHTML = '';
+
+        this.renderStudentList(filteredStudents, searchListContainer);
+    }
+    
 }
+
+const studentList = new StudentList('');
 
