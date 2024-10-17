984aade7eb191fe2c846802466084f0a2c5071e6
saldiii <evelinesaldivar@gmail.com>
2024-10-18 01:05:33 +0800

    added fetchWeatherByLocation

--- weather.js
+++ weather.js
@@ -62,4 +62,16 @@ class WeatherService extends WeatherApp {
         alert('Please enter a city name.');
     }
 
+    }
+
+    async fetchWeatherByLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                async (position) => {
+                    const { latitude, longitude } = position.coords;
+                    const data = await this.getWeatherDataByCoordinates(latitude, longitude);
+                }
+            }
+        }
+}
 
