87f7eab6de8378abd851d87c6285ea7a350cd4ec
saldiii <evelinesaldivar@gmail.com>
2024-10-18 00:51:08 +0800

    added APIinput

--- weather.js
+++ weather.js
@@ -15,6 +15,7 @@ class WeatherApp{
         this.description = document.getElementById('description');
         this.humidity = document.getElementById('humidity');
         this.windSpeed = document.getElementById('windSpeed');
-        
+        this.APIKeyInput = document.getElementById('apiInput');
+
     }
 }
